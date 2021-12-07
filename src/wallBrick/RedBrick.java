995cd33c8260c92eab78cf77b1f93dbab4a9cc58
boobiers <danielleong46@gmail.com>
2021-12-07 14:29:13 +0800

    Created a new brick called RedBrick, for the new level

--- src/wallBrick/RedBrick.java
+++ src/wallBrick/RedBrick.java
@@ -8,28 +8,28 @@ import java.awt.Point;
  * this class makes clay bricks which inherits from bricks
  *
  */
-public class ClayBrick extends Brick {
+public class RedBrick extends Brick {
 
-    private static final String NAME = "Clay Brick";
-    private static final Color DEF_INNER = new Color(178, 34, 34).darker();
+    private static final String NAME = "Red Brick";
+    private static final Color DEF_INNER = new Color(255, 102, 102).darker();
     private static final Color DEF_BORDER = Color.GRAY;
-    private static final int Red_STRENGTH = 1;
+    private static final int CLAY_STRENGTH = 3;
 
 
     /**
-     * this is to make clay brick
+     * this is to make Red brick
      * @param point the point (x,y) of the brick
      * @param size the dimension of the brick
      */
-    public ClayBrick(Point point, Dimension size){
-        super(NAME,point,size,DEF_BORDER,DEF_INNER,Red_STRENGTH);
+    public RedBrick(Point point, Dimension size){
+        super(NAME,point,size,DEF_BORDER,DEF_INNER,CLAY_STRENGTH);
     }
 
     /**
-     * this is to make the face for the clay brick
+     * this is to make the face for the Red brick
      * @param pos position of the brick
      * @param size the size of the brick
-     * @return clay brick
+     * @return Red brick
      */
     @Override
     protected Shape makeBrickFace(Point pos, Dimension size) {
@@ -38,7 +38,7 @@ public class ClayBrick extends Brick {
 
     /**
      * this is to get the clay brick
-     * @return clay brick
+     * @return Red brick
      */
     @Override
     public Shape getBrick() {
